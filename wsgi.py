d721a30608e2ccb3bf6962c8ba4ed3f2b57e8270
joseMirungu <josephirungu890@gmail.com>
2024-12-16 22:37:51 +0300

    Update deployment configuration

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-# Add the project directory to the Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# Add the current directory to Python path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from app import app
 
