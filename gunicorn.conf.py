8dee86e121f00029bcec39ad965b4188bd9abc19
joseMirungu <josephirungu890@gmail.com>
2024-12-16 22:22:22 +0300

    Update deployment configuration

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,7 @@ import multiprocessing
 
 bind = "0.0.0.0:10000"
 workers = multiprocessing.cpu_count() * 2 + 1
-threads = 2
-timeout = 120
-worker_class = "gthread"
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
