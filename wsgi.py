605dc5061478e9e95c1ab7a49f871e82b8ac7bf6
joseMirungu <josephirungu890@gmail.com>
2024-12-16 22:49:20 +0300

    Update deployment configuration

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-# Adjust the path to look in src directory
+# Adjust the path to look in server directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(current_dir, 'src')
-sys.path.insert(0, src_dir)
+server_dir = os.path.join(current_dir, 'server')
+sys.path.insert(0, server_dir)
 
 from app import app
 
